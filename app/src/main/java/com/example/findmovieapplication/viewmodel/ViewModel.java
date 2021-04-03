b078b3969e23c792abd4c2a5210333995deb46b9
bitaaz <bb_az@yahoo.com>
2021-04-03 13:48:03 +0430

    Use Repository for field Add Getter for list of movies

--- app/src/main/java/com/example/findmovieapplication/viewmodel/ViewModel.java
+++ app/src/main/java/com/example/findmovieapplication/viewmodel/ViewModel.java
@@ -1,4 +1,23 @@
 package com.example.findmovieapplication.viewmodel;
 
+import androidx.lifecycle.LiveData;
+
+import com.example.findmovieapplication.model.MovieModel;
+import com.example.findmovieapplication.repository.Repository;
+
+import java.util.List;
+
 public class ViewModel {
+
+    private Repository repository;
+
+    public ViewModel(){
+
+        repository = Repository.getInstance();
+    }
+
+    public LiveData<List<MovieModel>> getSearchedMovies(){
+
+        return repository.getSearchedMovies();
+    }
 }
