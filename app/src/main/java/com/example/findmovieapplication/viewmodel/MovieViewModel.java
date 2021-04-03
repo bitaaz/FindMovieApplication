237da60f41d46480284677f350439a7965e4f978
bitaaz <bb_az@yahoo.com>
2021-04-03 13:52:16 +0430

    Extend ViewModel Change class name from ViewModel to MovieViewModel

--- app/src/main/java/com/example/findmovieapplication/viewmodel/MovieViewModel.java
+++ app/src/main/java/com/example/findmovieapplication/viewmodel/MovieViewModel.java
@@ -1,17 +1,18 @@
 package com.example.findmovieapplication.viewmodel;
 
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
 
 import com.example.findmovieapplication.model.MovieModel;
 import com.example.findmovieapplication.repository.Repository;
 
 import java.util.List;
 
-public class ViewModel {
+public class MovieViewModel extends ViewModel {
 
     private Repository repository;
 
-    public ViewModel(){
+    public MovieViewModel(){
 
         repository = Repository.getInstance();
     }
