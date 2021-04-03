fa9f111f35401f3be19c946da4f4edac824760dd
bitaaz <bb_az@yahoo.com>
2021-04-03 13:47:35 +0430

    Use RemoteDataSourceApi for field Use Singleton pattern Add Getter for list of movies

--- app/src/main/java/com/example/findmovieapplication/repository/Repository.java
+++ app/src/main/java/com/example/findmovieapplication/repository/Repository.java
@@ -1,4 +1,42 @@
 package com.example.findmovieapplication.repository;
 
+import androidx.lifecycle.LiveData;
+
+import com.example.findmovieapplication.model.MovieModel;
+import com.example.findmovieapplication.request.RemoteDataSourceApi;
+
+import java.util.List;
+
 public class Repository {
+
+    private RemoteDataSourceApi remoteDataSourceApi;
+
+    private static Repository instance;
+
+    public static Repository getInstance(){
+
+        if (instance == null){
+
+            instance = new Repository();
+        }
+
+        return instance;
+    }
+
+    private Repository(){
+
+        remoteDataSourceApi = RemoteDataSourceApi.getInstance();
+    }
+
+
+    public LiveData<List<MovieModel>> getSearchedMovies(){
+
+        return remoteDataSourceApi.getSearchedMovies();
+
+    }
+
+
+
+
+
 }
