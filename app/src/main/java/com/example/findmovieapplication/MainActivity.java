e833ba4eaa0c8b2cd620ca97e04234bf86d73e23
bitaaz <bb_az@yahoo.com>
2021-04-03 13:53:00 +0430

    Define and Instantiate movieViewModel

--- app/src/main/java/com/example/findmovieapplication/MainActivity.java
+++ app/src/main/java/com/example/findmovieapplication/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.findmovieapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 
+import com.example.findmovieapplication.viewmodel.MovieViewModel;
+
 public class MainActivity extends AppCompatActivity {
+
+    private MovieViewModel movieViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        movieViewModel = new ViewModelProvider(this).get(MovieViewModel.class);
     }
 }
