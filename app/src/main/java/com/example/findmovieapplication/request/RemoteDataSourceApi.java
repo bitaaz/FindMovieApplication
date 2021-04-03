4d9f085808391aa261b606b701863ea85202bd73
bitaaz <bb_az@yahoo.com>
2021-04-03 13:45:21 +0430

    Add LiveData field Use Singleton pattern

--- app/src/main/java/com/example/findmovieapplication/request/RemoteDataSourceApi.java
+++ app/src/main/java/com/example/findmovieapplication/request/RemoteDataSourceApi.java
@@ -1,4 +1,36 @@
 package com.example.findmovieapplication.request;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.example.findmovieapplication.model.MovieModel;
+
+import java.util.List;
+
 public class RemoteDataSourceApi {
+
+    private MutableLiveData<List<MovieModel>> searchedMovies;
+
+    private static RemoteDataSourceApi instance;
+
+    public static RemoteDataSourceApi getInstance(){
+
+        if (instance == null){
+
+            instance = new RemoteDataSourceApi();
+        }
+
+        return instance;
+    }
+
+    private RemoteDataSourceApi(){
+
+        searchedMovies = new MutableLiveData<>();
+    }
+
+    public LiveData<List<MovieModel>> getSearchedMovies(){
+
+        return  searchedMovies;
+    }
+
 }
