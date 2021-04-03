99331b7d1f37599d4f0fefc611e77a8c004c327a
bitaaz <bb_az@yahoo.com>
2021-04-03 17:04:32 +0430

    Add constants for base url and api key

---
+++ app/src/main/java/com/example/findmovieapplication/utils/Constants.java
@@ -0,0 +1,7 @@
+package com.example.findmovieapplication.utils;
+
+public class Constants {
+
+    public static final String BASE_URL = "https://api.themoviedb.org";
+    public static final String API_KEY = "c9decc71b304ff349d4ae9c0553e9c45";
+}
