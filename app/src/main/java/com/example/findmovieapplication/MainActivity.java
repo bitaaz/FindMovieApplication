e94551f5977e071edf60c7e88c98b494eea26fdd
bitaaz <bb_az@yahoo.com>
2021-04-04 11:51:15 +0430

    add observer method initialize button

--- app/src/main/java/com/example/findmovieapplication/MainActivity.java
+++ app/src/main/java/com/example/findmovieapplication/MainActivity.java
@@ -1,15 +1,24 @@
 package com.example.findmovieapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.findmovieapplication.model.MovieModel;
 import com.example.findmovieapplication.viewmodel.MovieViewModel;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private MovieViewModel movieViewModel;
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +26,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         movieViewModel = new ViewModelProvider(this).get(MovieViewModel.class);
+
+        observer();
+
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                movieViewModel.searchMovieApi("fast", 1);
+
+            }
+        });
+    }
+
+    private void observer(){
+        movieViewModel.getSearchedMovies().observe(this, new Observer<List<MovieModel>>() {
+            @Override
+            public void onChanged(List<MovieModel> movieModels) {
+                if (movieModels != null){
+
+                    for (MovieModel movieModel: movieModels){
+
+                        Log.v("Tag", "Titles: " + movieModel.getTitle());
+                    }
+                }
+
+            }
+        });
     }
 }
