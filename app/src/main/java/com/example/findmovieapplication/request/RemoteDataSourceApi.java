8a71f2cccde4baf3ddca07d3cef520086d42b788
bitaaz <bb_az@yahoo.com>
2021-04-04 11:39:10 +0430

    use threads for retrieving searched movies from api

--- app/src/main/java/com/example/findmovieapplication/request/RemoteDataSourceApi.java
+++ app/src/main/java/com/example/findmovieapplication/request/RemoteDataSourceApi.java
@@ -1,11 +1,22 @@
 package com.example.findmovieapplication.request;
 
+import android.util.Log;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
+import com.example.findmovieapplication.AppExecutor;
 import com.example.findmovieapplication.model.MovieModel;
+import com.example.findmovieapplication.response.MovieSearchResponse;
+import com.example.findmovieapplication.utils.Constants;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+import retrofit2.Call;
+import retrofit2.Response;
 
 public class RemoteDataSourceApi {
 
@@ -49,13 +60,80 @@ public class RemoteDataSourceApi {
             this.cancelRequest = false;
         }
 
+        private Call<MovieSearchResponse> getMovies(String query, int pageNumber){
 
+            return RetrofitBuilder.getRetrofitInterface().searchedMovies(Constants.API_KEY, query, pageNumber);
+        }
+
+        private void cancelRequest(){
+            cancelRequest = true;
+            Log.v("Tag", "Cancel search request ");
+        }
 
 
         @Override
         public void run() {
 
+            try {
+
+                Response response = getMovies(query, pageNumber).execute();
+                if (cancelRequest){
+                    return;
+                }
+
+                if (response.code() == 200){
+
+                    List<MovieModel> list = new ArrayList<>(((MovieSearchResponse)response.body()).getMovies());
+
+                    if (pageNumber == 1){
+
+                        searchedMovies.postValue(list);
+                    }
+                    else {
+
+                        List<MovieModel> currentMovies = searchedMovies.getValue();
+                        currentMovies.addAll(list);
+                        searchedMovies.postValue(currentMovies);
+                    }
+                }
+
+                else {
+
+                    String error = response.errorBody().toString();
+                    Log.v("Tag", "Error: " + error);
+                    searchedMovies.postValue(null);
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                searchedMovies.postValue(null);
+            }
+
+
+
         }
+    }
+
+    public void searchMovieApi(String query, int pageNumber){
+
+        if (searchedMoviesRunnable != null){
+
+            searchedMoviesRunnable = null;
+        }
+
+        searchedMoviesRunnable = new SearchedMoviesRunnable(query,pageNumber);
+
+        //future for using thread
+        final Future handler = AppExecutor.getInstance().getNetworkIO().submit(searchedMoviesRunnable);
+
+        AppExecutor.getInstance().getNetworkIO().schedule(new Runnable() {
+            @Override
+            public void run() {
+
+                handler.cancel(true);
+
+            }
+        }, 3000, TimeUnit.MICROSECONDS);
     }
 
 }
