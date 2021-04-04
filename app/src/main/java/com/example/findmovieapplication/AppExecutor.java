d0b90578ee5fd09320d0c992ddf54a06f1de687d
bitaaz <bb_az@yahoo.com>
2021-04-04 11:36:54 +0430

    create AppExecutor class for scheduling threads

---
+++ app/src/main/java/com/example/findmovieapplication/AppExecutor.java
@@ -0,0 +1,26 @@
+package com.example.findmovieapplication;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
+public class AppExecutor {
+
+    private static AppExecutor instance;
+
+    public static AppExecutor getInstance(){
+
+        if (instance == null){
+
+            instance = new AppExecutor();
+        }
+
+        return instance;
+    }
+
+    private final ScheduledExecutorService networkIO = Executors.newScheduledThreadPool(3);
+
+    public ScheduledExecutorService getNetworkIO(){
+
+        return networkIO;
+    }
+}
