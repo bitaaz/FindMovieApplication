474fa2882a5730c73bc11ed61f655975f81c4cf0
bitaaz <bb_az@yahoo.com>
2021-04-05 15:40:43 +0430

    add runnable for getting popular movies add popMovieApi method

--- app/src/main/java/com/example/findmovieapplication/request/RemoteDataSourceApi.java
+++ app/src/main/java/com/example/findmovieapplication/request/RemoteDataSourceApi.java
@@ -24,6 +24,10 @@ public class RemoteDataSourceApi {
 
     private SearchedMoviesRunnable searchedMoviesRunnable;
 
+    private MutableLiveData<List<MovieModel>> popMovies;
+
+    private PopMoviesRunnable popMoviesRunnable;
+
 
     private static RemoteDataSourceApi instance;
 
@@ -40,11 +44,17 @@ public class RemoteDataSourceApi {
     private RemoteDataSourceApi(){
 
         searchedMovies = new MutableLiveData<>();
+        popMovies = new MutableLiveData<>();
     }
 
     public LiveData<List<MovieModel>> getSearchedMovies(){
 
         return  searchedMovies;
+    }
+
+    public LiveData<List<MovieModel>> getPopMovies(){
+
+        return popMovies;
     }
 
 
@@ -136,4 +146,81 @@ public class RemoteDataSourceApi {
         }, 3000, TimeUnit.MILLISECONDS);
     }
 
+
+    private class PopMoviesRunnable implements Runnable{
+
+        private int pageNumber;
+        boolean cancelRequest;
+
+        public PopMoviesRunnable(int pageNumber) {
+            this.pageNumber = pageNumber;
+            cancelRequest = false;
+        }
+
+
+        private Call<MovieSearchResponse> getPopMovie(int pageNumber){
+
+            return RetrofitBuilder.getRetrofitInterface().popMovies(Constants.API_KEY, pageNumber);
+
+        }
+
+        @Override
+        public void run() {
+
+            try {
+
+                Response response = getPopMovie(pageNumber).execute();
+                if (cancelRequest){
+
+                    return;
+                }
+                if (response.code() == 200){
+
+                    List<MovieModel> popList = new ArrayList<>(((MovieSearchResponse)response.body()).getMovies());
+
+                    if (pageNumber == 1){
+
+                        popMovies.postValue(popList);
+                    }
+                    else {
+
+                        List<MovieModel> currentPopMovies = popMovies.getValue();
+                        currentPopMovies.addAll(popList);
+                        popMovies.postValue(currentPopMovies);
+                    }
+
+                }
+                else {
+
+                    Log.v("Tag", "Error: " + response.errorBody().toString());
+                    popMovies.postValue(null);
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                popMovies.postValue(null);
+            }
+
+        }
+    }
+
+    public void popMovieApi(int pageNumber){
+
+        if (popMoviesRunnable != null){
+            popMoviesRunnable = null;
+        }
+
+        popMoviesRunnable = new PopMoviesRunnable(pageNumber);
+
+        final Future handler = AppExecutor.getInstance().getNetworkIO().submit(popMoviesRunnable);
+
+        AppExecutor.getInstance().getNetworkIO().schedule(new Runnable() {
+            @Override
+            public void run() {
+
+                handler.cancel(true);
+
+            }
+        },3000, TimeUnit.MILLISECONDS);
+    }
 }
